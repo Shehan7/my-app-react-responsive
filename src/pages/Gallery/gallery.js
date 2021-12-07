222ca1991d02c6d4eb3375b59ee1322c56e54533
Shehan Somaweera <shehan.somaweera@gmail.com>
2021-12-07 14:44:01 +0530

    Changed the gallery

--- src/pages/Gallery/gallery.js
+++ src/pages/Gallery/gallery.js
@@ -1,28 +1,42 @@
+// src/reusable/image-gallery.component.js
 import React from "react";
-import { _IMAGES } from "./data";
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
-import Style from '../About/about.module.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-class ReactImageGalleryTest extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: []
+class ImageGallaryComponent extends React.Component {
+    render() {
+        return (
+            <div className="container mt-5 mb-5 ">
+                
+                <Carousel autoPlay interval="2000" transitionTime="800">
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=1" />
+                        <p className="legend">Gallery Img No.01</p>
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=2" />
+                        <p className="legend">Gallery Img No.02</p>
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=3" />
+                        <p className="legend">Gallery Img No.03</p>
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=4" />
+                        <p className="legend">Gallery Img No.04</p>
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=5" />
+                        <p className="legend">Gallery Img No.05</p>
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/700/400?img=6" />
+                        <p className="legend">Gallery Img No.06</p>
+                    </div>
+                </Carousel>
+            </div>
+        )
     };
-  }
-
-  render() {
-    return (
-      <div>
-          <div className={Style.headAbout}>
-                <h1>Gallery</h1>
-         </div>
-         <div className='container mt-5 mb-5'>
-            <ImageGallery showThumbnails={true} items={_IMAGES} />
-        </div>
-      </div>
-    );
-  }
 }
-export default ReactImageGalleryTest;
+
+export default ImageGallaryComponent;
